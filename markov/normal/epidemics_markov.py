1b2abbd49add1140402b00da3274b08cc5db0213
Gilberto Medeiros Nakamura <gilberto.nakamura@gmail.com>
2017-09-05 23:01:15 -0300

    added markov normal

--- markov/normal/epidemics_markov.py
+++ markov/normal/epidemics_markov.py
@@ -1,9 +1,26 @@
 import numpy as np
 
-def apply_transition_matrix(config,transition):
-    n=len(config)
+
+
+
+def update(configs,transition,params):
+    final={}
+
+    for config in configs:
+        val=configs[config]
+        new_configs=apply_transition_matrix(config,transition,params)
+        for new_config in new_configs:
+            if new_config not in final:
+                final[new_config]=0
+            final[new_config]+=new_configs[new_config]*val
+    return final
+
+#==================================================
+
+def apply_transition_matrix(config,transition,params):
+    N=len(config)
     new={}
-    for k in range(n):
+    for k in range(N):
         change=  transition[config[k]]
         #
         # create new config
@@ -15,8 +32,8 @@ def apply_transition_matrix(config,transition):
         if word not in new:
             new[word]=0
         new[word]+=change[1] # adds new contribution
-    for i in range(n):
-        for j in range(n):        
+    for i in range(N):
+        for j in range(N):        
             pair=config[i]+config[j]
             change=transition[pair]
             if change[0] is not None:
@@ -25,11 +42,20 @@ def apply_transition_matrix(config,transition):
                 if word not in new:
                     new[word]=0
                 new[word]+=change[1] # adds new contribution
+                
+    num_infected = config.count('1')
+
+    #
+    # params = (alpha,gamma,gamma_bar)
+    #
+    new[config]= 1.0 - (N-num_infected)*(num_infected*params[0]+params[2])-num_infected*params[1]
+                 
     return new
 
-def update(configs,transition):
-    new={}
-    for config in configs
+
+
+        
+#==================================================        
 
 
 
@@ -40,10 +66,14 @@ if __name__ == '__main__':
     N = 4
     gamma = 0.3/N
     alpha = 1.0/(N*N)
-    gamma_bar = 1.0/(N*N)
+    gamma_bar = 0.1/(N*N)
+    
+    params=(alpha,gamma,gamma_bar)
     transition={ '0':('1',gamma_bar),'1':('0',gamma),
                  '00':(None,0),'11':(None,0),
                  '10':('11',alpha),'01':(None,0) }
     
     
-    
+    current={'1111':1}
+    current=update(current,transition,params); print(sum(current.values()))
+
